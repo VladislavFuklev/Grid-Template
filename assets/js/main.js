98a094d0bd5ed4dbf2e60cb419d3132a8a1fe4b8
Vladislav <vladfyklev@gmail.com>
2022-05-28 17:18:31 +0300

    закончили верстку в классе

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,11 @@ $(document).ready(function () {
   });
 
 });
+
+
+$(document).ready(function(){
+	$('.header-navbar__btn').click(function(){
+		$(this).toggleClass('header-navbar__btn--active');
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
+	});
+});
